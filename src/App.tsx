c86e84a6914c35bef562aa5838f0fc6c1e389ea5
Mohammed Kharoda <kharodawalam@gmail.com>
2023-04-06 13:06:32 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import { Routes, Router, Route } from "react-router-dom";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
